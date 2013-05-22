2ecca548e1e720a8ca42eb05a1bed312c4662f2e
Paul Sutcliffe <paul@kosmyka.com>
2013-05-21 19:23:47 -0500

    App created with a home controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kaptive::Application.config.secret_token = '1c87f15701b05859dddc3ed6c1eb476da1ca78d7170175bda98c8605036392cc76231fd14cdf3e1c5a4642ec2815003ca0ba2972ca48bd49ad710fb07106d899'
