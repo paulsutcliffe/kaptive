5ec12ef8be736368bb0ba36479a51bf4a8f7872d
Ricardo <marcorcp1@hotmail.com>
2013-05-24 00:51:30 -0500

    productos show 95% terminado, falta carrusel y hover

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  #--------------------------------------------------------
+  # Paragraph
+  #--------------------------------------------------------
+  def paragraph(text)
+    "#{sanitize(simple_format(text), :tags => %w(p i br b))}".html_safe
+  end
+
+  def paragraph_truncate(text, length)
+    "#{sanitize(simple_format(truncate(text, :length => length)), :tags => %w(p i br b))}".html_safe
+  end
+
 end
