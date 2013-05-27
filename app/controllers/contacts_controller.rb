8de6145d2ea0c30727ef49bc0cc89127db2793f3
Paul Sutcliffe <paul@kosmyka.com>
2013-05-27 01:39:35 -0500

    Contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,4 +5,20 @@ class ContactsController < InheritedResources::Base
     @contacts = Contact.all
     @last_contact = Contact.last
   end
+
+  def create
+    @contact = Contact.new(params[:contacto])
+
+    create! do |success, failure|
+      success.html do
+        flash[:notice] = "Your message was sent."
+        redirect_to new_contact_path
+      end
+
+      failure.html do
+        flash[:notice] = "There was an error, message was not sent."
+        render :action => "new"
+      end
+    end
+  end
 end
