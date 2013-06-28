7658a7df2647a09a7586f468c9ba8309da7f903a
Paul Sutcliffe <paul@kosmyka.com>
2013-06-28 02:37:03 -0500

    devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Kaptive::Application.routes.draw do
 
   match "/underconstruction" => "under_construction#index", :via => :get
 
-  devise_for :admins
+  devise_for :admins, :path => "cms", :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'secret', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'cmon_let_me_in' }
 
   match "/about_us" => "about#show", :via => :get, :defaults => { :id => '1' }
 
